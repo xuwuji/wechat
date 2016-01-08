65d92d11c7a9510e26502ed3e264e87582dcf920
Jameslily <xuwuji@192.168.1.101>
2016-01-09 00:54:38 +0800

    1-8

---
+++ xuwuji-wechat/src/main/java/com/xuwuji/wechat/model/ResultMessage.java
@@ -0,0 +1,13 @@
+package com.xuwuji.wechat.model;
+
+/**
+ * Interface for Message Back to User
+ * 
+ * @author wuxu
+ * @time 2016Äê1ÔÂ8ÈÕ
+ */
+public interface ResultMessage {
+	String getMsgType();
+
+	void setContent(String content);
+}
---
+++ xuwuji-wechat/src/main/java/com/xuwuji/wewchat/tuling/TulingGetResult.java
@@ -0,0 +1,18 @@
+package com.xuwuji.wewchat.tuling;
+
+import com.xuwuji.wechat.util.HttpUtil;
+
+/**
+ * Get the result from tuling rebort based on the input key
+ * 
+ * @author xuwuji
+ *
+ */
+public class TulingGetResult {
+	private static String URL = "http://www.tuling123.com/openapi/api?key=e99e29fe48c3c8c1c6f1f342c0362761&info=";
+
+	public static String getResult(String key) throws Exception {
+		return HttpUtil.GetTuLing(key);
+	}
+
+}
